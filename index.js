b42000b469f4232a6dc64e9dcb9c4ee31aa8e4e6
Kiabellla <scarlett.jimenez26@compscihigh.org>
2025-10-05 23:31:25 +0000

    Third commit

--- index.js
+++ index.js
@@ -36,17 +36,22 @@ delete song.artist
 // ----------------------------------
 // NESTED ARRAYS & OBJECTS SECTION
 // Task 8: Create an object called 'album' with properties song's album's 'name', 'year', and 'songs' (leave 'songs' as empty array for now)
-
+let album = {
+    name: "Starboy",
+    year: 2016,
+    songs: []
+}
 
 // Task 9: Add your song object (task 5) to the empty songs array in the album object
-
+album.songs.push(song)
 
 // Console log the length of the album's songs array to confirm it worked
+console.log("Number of songs:", album.songs.length)
 
 // Task 10: Update the title of the song to be all lowercase. 
 // Console log the album object to check your change
-
-
+album.songs[0].title = album.songs[0].title.toLowerCase()
+console.log("Updated:", album)
 // Commit your changes with an appropriate message.
 // ----------------------------------
 // EXTRA CREDIT
